8fc0f18cd3552009ad39a15945c5f11d11849296
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-05 17:02:14 +0100

    Minor tweaks

--- test/test.js
+++ test/test.js
@@ -6,21 +6,21 @@ var rm = require('rimraf');
 var test = require('ava');
 
 test('transpile CSS', function (t) {
-    t.plan(4);
+	t.plan(4);
 
-    var css = 'body {\n  width: 50px;\n}';
-    var dest = path.join(__dirname, 'tmp/test.css');
+	var css = 'body {\n  width: 50px;\n}';
+	var dest = path.join(__dirname, 'tmp/test.css');
 
-    fs.readFile(dest, 'utf8', function (err, data) {
-        t.assert(!err, err);
-        t.assert(data === css);
+	fs.readFile(dest, 'utf8', function (err, data) {
+		t.assert(!err, err);
+		t.assert(data === css);
 
-        rm(path.join(__dirname, 'tmp'), function (err) {
-            t.assert(!err, err);
+		rm(path.join(__dirname, 'tmp'), function (err) {
+			t.assert(!err, err);
 
-            rm(path.join(__dirname, '../tmp'), function (err) {
-                t.assert(!err, err);
-            });
-        });
-    });
+			rm(path.join(__dirname, '../tmp'), function (err) {
+				t.assert(!err, err);
+			});
+		});
+	});
 });
