6a4e96954d4625cc30de9f2668641375b3e9f42a
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 18:34:02 +0100

    Tweaks

--- index.js
+++ index.js
@@ -3,13 +3,6 @@
 var Filter = require('broccoli-filter');
 var myth = require('myth');
 
-/**
- * Initialize `MythFilter`
- *
- * @param {Object} inputTree
- * @api public
- */
-
 function MythFilter(inputTree) {
 	if (!(this instanceof MythFilter)) {
 		return new MythFilter(inputTree);
@@ -18,33 +11,12 @@ function MythFilter(inputTree) {
 	this.inputTree = inputTree;
 }
 
-/**
- * Create object
- */
-
 MythFilter.prototype = Object.create(Filter.prototype);
 MythFilter.prototype.constructor = MythFilter;
-
-/**
- * Extensions
- */
-
 MythFilter.prototype.extensions = ['css'];
 MythFilter.prototype.targetExtension = 'css';
-
-/**
- * Process CSS
- *
- * @param {String} str
- * @api public
- */
-
 MythFilter.prototype.processString = function (str) {
 	return myth(str);
 };
-
-/**
- * Module exports
- */
 
 module.exports = MythFilter;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# broccoli-myth [![Build Status](https://travis-ci.org/kevva/broccoli-myth.svg?branch=master)](https://travis-ci.org/kevva/broccoli-myth)
+# broccoli-myth [![Build Status](http://img.shields.io/travis/kevva/broccoli-myth.svg?style=flat)](https://travis-ci.org/kevva/broccoli-myth)
 
 > Preprocess CSS with [Myth](https://github.com/segmentio/myth)
 
